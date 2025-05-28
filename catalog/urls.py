50f4abba19ae557f92f77087b667c82de0a51448
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-05-28 20:02:52 +0400

    RESULT 1

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ from catalog.views import contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
 ]
