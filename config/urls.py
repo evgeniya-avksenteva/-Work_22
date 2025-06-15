35c874ddea457004242b38b0dd46544299e97cd5
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-06-16 00:38:23 +0300

    RESULT develop hw

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
