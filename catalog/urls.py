3dbe968819c4b387568d618e51bbb1a039f6ae41
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-06-20 23:50:39 +0300

    HW pk

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 # from catalog.views import home
 # from catalog.views import contacts
-from catalog.views import index
+from catalog.views import product_list, product_detail
 
 
 app_name = CatalogConfig.name
@@ -13,5 +13,7 @@ app_name = CatalogConfig.name
 # ]
 
 urlpatterns = [
-    path("", index)
+    path("", product_list, name='product_list'),
+    path("products/<int:pk>", product_detail, name='product_detail')
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from gc import get_objects
+from itertools import product
+
+from catalog.models import Product
+from django.shortcuts import render, get_object_or_404
 
 
 # def home(request):
@@ -8,5 +12,12 @@ from django.shortcuts import render
 # def contacts(request):
 #     return render(request, "contacts.html")
 
-def index(request):
-    return render(request, "base.html")
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "product_list.html", context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context)
