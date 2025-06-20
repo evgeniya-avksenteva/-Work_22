97a27c20fc1c989326bbb65f3fd51f636eaf28cb
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-06-20 22:05:00 +0300

    HW 24 bootstar

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home
-from catalog.views import contacts
+# from catalog.views import home
+# from catalog.views import contacts
+from catalog.views import index
+
 
 app_name = CatalogConfig.name
 
+# urlpatterns = [
+#     path("", home, name="home"),
+#     path("contacts/", contacts, name="contacts"),
+# ]
+
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
+    path("", index)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, "home.html")
+# def home(request):
+#     return render(request, "home.html")
+#
+#
+# def contacts(request):
+#     return render(request, "contacts.html")
 
-
-def contacts(request):
-    return render(request, "contacts.html")
+def index(request):
+    return render(request, "base.html")
