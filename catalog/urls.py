6c8c9b5a0b6eb4e5e86f93a395a946a08b3a606a
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-06-25 23:23:36 +0300

    HW 25

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-# from catalog.views import home
-# from catalog.views import contacts
-from catalog.views import product_list, product_detail
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 
 app_name = CatalogConfig.name
 
-# urlpatterns = [
-#     path("", home, name="home"),
-#     path("contacts/", contacts, name="contacts"),
-# ]
 
 urlpatterns = [
-    path("", product_list, name='product_list'),
-    path("products/<int:pk>/", product_detail, name='product_detail')
+    path("", ProductListView.as_view(), name='product_list'),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
+    path("product/create", ProductCreateView.as_view(), name='product_create'),
+    path("product/<int:pk>/update", ProductUpdateView.as_view(), name='product_update'),
+    path("product/<int:pk>/delete", ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,31 @@
 from gc import get_objects
 from itertools import product
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from catalog.models import Product
 from django.shortcuts import render, get_object_or_404
 
 
-# def home(request):
-#     return render(request, "home.html")
-#
-#
-# def contacts(request):
-#     return render(request, "contacts.html")
+class ProductListView(ListView):
+    model = Product
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "product_list.html", context)
+class ProductDetailView(DetailView):
+    model = Product
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ("name", "description", "image", "category", "purchase_price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("name", "description", "image", "category", "purchase_price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
